5122a9aed26695b395c6f3047c5ccb6369e6c1c0
Piter920120 <rynek0@onet.eu>
2019-03-05 20:43:23 +0100

    added color

--- homework/3/src/App.js
+++ homework/3/src/App.js
@@ -1,48 +1,64 @@
-import React, { Component } from 'react';
+
+  
+import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-
   state = {
     people: []
-  }
-
-  componentDidMount(){
+  };
+  componentDidMount() {
     fetch(process.env.PUBLIC_URL + "data/people.json")
-    .then(response => response.json())
-    .then(people => this.setState({ people }))
-      .catch(error => console.log(error))
+      .then(response => response.json())
+      .then(people => this.setState({ people }))
+      .catch(err => console.log(err));
   }
+   
+
+ handleToggleFav = personId => {
+   this.setState({
+     people: this.state.people.map(person => {
+      return (
+        personId === person.id ?
+       {
+         ...person,
+         isFavorite: !(person.isFavorite)
+       }
+         : person
+     )
+       })
+     })
+ }
+ 
 
   render() {
     return (
       <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Phone</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.people.map(person => {
-            return (
-              <tr 
-              className={person.isFavorite ? "highlithted" : ""}
-              key={person.id}>
-              <td>{person.name}</td>
-              <td>{person.surname}</td>
-              <td>{person.phone}</td>
-              <td><button>Toggle favorite</button></td>
-              </tr>
-            )
-          })}
-         
-        </tbody>
-      </table>
-       
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Surname</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.people.map(person => {
+              return (
+                <tr
+                  className={person.isFavorite ? "highlighted" : ""}
+                  key={person.id}
+                >
+                  <td>{person.name}</td>
+                  <td>{person.surname}</td>
+                  <td>{person.phone}</td>
+                  <td><button onClick={() => this.handleToggleFav(person.id)}>Toggle favorite</button></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
