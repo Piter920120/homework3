7d9a431c3a7718aa271c725ff595fab3803509bb
Piter920120 <rynek0@onet.eu>
2019-03-05 21:05:55 +0100

    romove

--- homework/3/src/App.js
+++ homework/3/src/App.js
@@ -1,7 +1,7 @@
 
   
 import React, { Component } from "react";
-import AddPeople from './AddPeople'
+import AddPeople from "./AddPeople";
 import "./App.css";
 
 class App extends Component {
@@ -10,53 +10,55 @@ class App extends Component {
   };
   componentDidMount() {
     fetch(process.env.PUBLIC_URL + "data/people.json")
-      .then(response => response.json())
+      .then(res => res.json())
       .then(people => this.setState({ people }))
       .catch(err => console.log(err));
   }
-   
 
- handleToggleFav = personId => {
-   this.setState({
-     people: this.state.people.map(person => {
-      return (
-        personId === person.id ?
-       {
-         ...person,
-         isFavorite: !(person.isFavorite)
-       }
-         : person
-     )
-       })
-     })
- }
-
-addName = (name, surname, phone) => {
-  if (name && surname && phone) {
+  handleToggleFav = personId => {
     this.setState({
-     people: this.state.people.concat({
-       id: Date.now(),
-       name: name,
-       surname: surname,
-       phone: phone,
-       isFavorite: false
-     })
-    })
-   }
- }
- 
+      people: this.state.people.map(person => {
+        return personId === person.id
+          ? {
+              ...person,
+              isFavorite: !person.isFavorite
+            }
+          : person;
+      })
+    });
+  };
+
+  addName = (name, surname, phone) => {
+    if (name && surname && phone) {
+      this.setState({
+        people: this.state.people.concat({
+          id: Date.now(),
+          name: name,
+          surname: surname,
+          phone: phone,
+          isFavorite: false
+        })
+      });
+    }
+  };
+
+  handleRemove = personId => {
+      this.setState({
+        people: this.state.people.filter(person => personId !== person.id)
+      })
+  }
 
   render() {
     return (
       <div className="App">
-        <AddPeople addName={this.addName}/>
+        <AddPeople addName={this.addName} />
         <table>
           <thead>
             <tr>
               <th>Name</th>
               <th>Surname</th>
               <th>Phone</th>
-              <th></th>
+              <th />
             </tr>
           </thead>
           <tbody>
@@ -69,7 +71,14 @@ addName = (name, surname, phone) => {
                   <td>{person.name}</td>
                   <td>{person.surname}</td>
                   <td>{person.phone}</td>
-                  <td><button onClick={() => this.handleToggleFav(person.id)}>Toggle favorite</button></td>
+                  <td>
+                    <button onClick={() => this.handleToggleFav(person.id)}>
+                      Toggle favorite
+                    </button>
+                  </td>
+                  <td>
+                    <button onClick={(() => this.handleRemove(person.id))}>Remove contact</button>
+                  </td>
                 </tr>
               );
             })}
